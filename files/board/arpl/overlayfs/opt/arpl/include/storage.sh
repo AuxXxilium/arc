01ff0de1a8c7fa7c3d7cf4d25bfc828762f23bb9
AuxXxilium <info@visionz.media>
2023-03-23 21:08:16 +0100

    arc: storage: rewrite script
    Signed-off-by: AuxXxilium <info@visionz.media>

--- files/board/arpl/overlayfs/opt/arpl/include/storage.sh
+++ files/board/arpl/overlayfs/opt/arpl/include/storage.sh
@@ -3,12 +3,11 @@ function getmap() {
   [ -n "SATAPORTMAP" ] && SATAPORTMAP=0
   rm -f ${TMP_PATH}/drives
   touch ${TMP_PATH}/drives
-  sleep 1
-  # Get Number of Sata Drives
-  if [ "${SATACONTROLLER}" -gt 0 ]; then
-    pcis=$(lspci -nnk | grep -ie "\[0106\]" | awk '{print $1}')
+  # Get Number of Drives per Controller
+  if [ "${SATACONTROLLER}" -gt 0 ] || [ "${SCSICONTROLLER}" -gt 0 ]; then
+    pcis=$(lspci -nnk | grep -ie "\[0104\]" -ie "\[0106\]" -ie "\[0107\]" | awk '{print $1}')
     [ ! -z "$pcis" ]
-    # loop through SATA controllers
+    # loop through controllers
     for pci in $pcis; do
     # get attached block devices (exclude CD-ROMs)
     DRIVES=$(ls -la /sys/block | fgrep "${pci}" | grep -v "sr.$" | wc -l)
@@ -19,32 +18,13 @@ function getmap() {
     echo -n "${DRIVES}" >> ${TMP_PATH}/drives
     done
   fi
-  # Get Number of Raid/SCSI Drives
-  if [ "${SCSICONTROLLER}" -gt 0 ]; then
-    pcis=$(lspci -nnk | grep -ie "\[0104\]" -ie "\[0107\]" | awk '{print $1}')
-    [ ! -z "$pcis" ]
-    # loop through non-SATA controllers
-    for pci in $pcis; do
-    # get attached block devices (exclude CD-ROMs)
-      DRIVES=$(ls -la /sys/block | fgrep "${pci}" | grep -v "sr.$" | wc -l)
-    if [ "${DRIVES}" -gt 8 ]; then
-      DRIVES=8
-      WARNON=1
-    fi
-    echo -n "${DRIVES}" >> ${TMP_PATH}/drives
-    done
-  fi
   # Write to config
       SATAPORTMAP=$(awk '{print$1}' ${TMP_PATH}/drives)
-      if [ "${SATAPORTMAP}" -gt 10 ]; then
-        writeConfigKey "cmdline.SataPortMap" "${SATAPORTMAP}" "${USER_CONFIG_FILE}"
-        writeConfigKey "cmdline.DiskIdxMap" "0" "${USER_CONFIG_FILE}"
-      fi
       if [ "${SATAPORTMAP}" -lt 8 ]; then
         SATAPORTMAP=8
+      fi
         writeConfigKey "cmdline.SataPortMap" "${SATAPORTMAP}" "${USER_CONFIG_FILE}"
         writeConfigKey "cmdline.DiskIdxMap" "0" "${USER_CONFIG_FILE}"
-      fi
 }
 
 # Check for Controller
