1217a095c28e375c0121eadf8d8bbcb91565a084
AuxXxilium <info@auxxxilium.tech>
2023-05-28 13:18:04 +0200

    arc: change ip and mac handling
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- files/board/arpl/overlayfs/opt/arpl/init.sh
+++ files/board/arpl/overlayfs/opt/arpl/init.sh
@@ -65,9 +65,6 @@ if [ -d "${CACHE_PATH}/patch" ]; then
   ln -s "${CACHE_PATH}/patch" "${PATCH_PATH}"
 fi
 
-# Get first MAC address
-ETHX=(`ls /sys/class/net/ | grep eth`)  # real network cards list
-
 # If user config file not exists, initialize it
 if [ ! -f "${USER_CONFIG_FILE}" ]; then
   touch "${USER_CONFIG_FILE}"
@@ -98,18 +95,25 @@ if [ ! -f "${USER_CONFIG_FILE}" ]; then
   # writeConfigKey "cmdline.mac1" "`cat /sys/class/net/${ETHX[0]}/address | sed 's/://g'`" "${USER_CONFIG_FILE}"
 fi
 
+# Get MAC address
+ETHX=(`ls /sys/class/net/ | grep eth`)  # real network cards list
 for N in $(seq 1 ${#ETHX[@]}); do
   MACR="`cat /sys/class/net/${ETHX[$(expr ${N} - 1)]}/address | sed 's/://g'`"
+  # Initialize with real MAC
+  writeConfigKey "device.mac${N}" "${MACR}" "${USER_CONFIG_FILE}"
   # Set custom MAC if defined
   MACF="`readConfigKey "cmdline.mac${N}" "${USER_CONFIG_FILE}"`"
   if [ -n "${MACF}" -a "${MACF}" != "${MACR}" ]; then
     MAC="${MACF:0:2}:${MACF:2:2}:${MACF:4:2}:${MACF:6:2}:${MACF:8:2}:${MACF:10:2}"
     echo "`printf "Setting %s MAC to %s" "${ETHX[$(expr ${N} - 1)]}" "${MAC}"`"
-    ifconfig ${ETHX[$(expr ${N} - 1)]} hw ether ${MAC} >/dev/null 2>&1 && \
-      (/etc/init.d/S41dhcpcd restart >/dev/null 2>&1 &) || true
+    ifconfig ${ETHX[$(expr ${N} - 1)]} down >/dev/null 2>&1
+    ifconfig ${ETHX[$(expr ${N} - 1)]} hw ether ${MAC} >/dev/null 2>&1
+    ifconfig ${ETHX[$(expr ${N} - 1)]} up >/dev/null 2>&1
+    /etc/init.d/S41dhcpcd restart >/dev/null 2>&1
+  elif [ -z "${MACF}" ]; then
+    # Write real Mac to cmdline config
+    writeConfigKey "cmdline.mac${N}" "${MACR}" "${USER_CONFIG_FILE}"
   fi
-  # Initialize with real MAC
-  writeConfigKey "device.mac${N}" "${MACR}" "${USER_CONFIG_FILE}"
   # Enable Wake on Lan, ignore errors
   ethtool -s ${ETHX[$(expr ${N} - 1)]} wol g 2>/dev/null
 done
