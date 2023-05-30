7b8f11827347a54d073b140ed419f9b468fc1044
AuxXxilium <info@auxxxilium.tech>
2023-05-30 18:24:12 +0200

    arc: change dsm version order
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- files/board/arpl/overlayfs/opt/arpl/arc.sh
+++ files/board/arpl/overlayfs/opt/arpl/arc.sh
@@ -204,18 +204,18 @@ function arcbuild() {
   while true; do
     dialog --clear --backtitle "`backtitle`" \
       --menu "Choose a DSM Version" 0 0 0 \
-      1 "DSM 7.1.1" \
-      2 "DSM 7.2.0" \
+      1 "DSM 7.2.0" \
+      2 "DSM 7.1.1" \
     2>${TMP_PATH}/resp
     [ $? -ne 0 ] && return
     resp=$(<${TMP_PATH}/resp)
     [ -z "${resp}" ] && return
     if [ "${resp}" = "1" ]; then
-      BUILD="42962"
+      BUILD="64561"
       writeConfigKey "build" "${BUILD}" "${USER_CONFIG_FILE}"
       break
     elif [ "${resp}" = "2" ]; then
-      BUILD="64561"
+      BUILD="42962"
       writeConfigKey "build" "${BUILD}" "${USER_CONFIG_FILE}"
       break
     fi
