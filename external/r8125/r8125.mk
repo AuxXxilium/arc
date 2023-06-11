e87ecc266e704dee9409b20d7315bc1b3fe63b37
AuxXxilium <info@auxxxilium.tech>
2023-06-11 18:33:11 +0200

    arc: buildroot: add r8169/r8125 for linux 6.1 kernel

--- external/r8125/r8125.mk
+++ external/r8125/r8125.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-R8125_VERSION = 4b4f60bed1e9dee6ea4394482dcb7449ade95a4a
-R8125_SITE = $(call github,AuxXxilium,r8125,$(R8125_VERSION))
+R8125_VERSION = a44d1347a47fe627754583fcb012b330aec01e84
+R8125_SITE = $(call github,wjz304,r8125,$(R8125_VERSION))
 R8125_LICENSE = GPL-2.0
 
 $(eval $(kernel-module))
