eb0c456686ce265a83ca0092c0d1b7f411a15468
AuxXxilium <info@auxxxilium.tech>
2023-07-01 18:52:54 +0200

    arc: cleanup
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- files/board/arpl/overlayfs/opt/arpl/include/modules.sh
+++ files/board/arpl/overlayfs/opt/arpl/include/modules.sh
@@ -12,7 +12,7 @@ function getAllModules() {
   tar -zxf "${MODULES_PATH}/${PLATFORM}-${KVER}.tgz" -C "${TMP_PATH}/modules"
   # Get list of all modules
   for F in $(ls ${TMP_PATH}/modules/*.ko); do
-    X=`basename ${F}`
+    X=$(basename ${F})
     M=${X:0:-3}
     DESC=$(modinfo ${F} | awk -F':' '/description:/{ print $2}' | awk '{sub(/^[ ]+/,""); print}')
     [ -z "${DESC}" ] && DESC="${X}"
@@ -53,4 +53,33 @@ function delToModules() {
   tar -zxf "${MODULES_PATH}/${PLATFORM}-${KVER}.tgz" -C "${TMP_PATH}/modules"
   rm -f ${TMP_PATH}/modules/${KONAME}
   tar -zcf "${MODULES_PATH}/${PLATFORM}-${KVER}.tgz" -C "${TMP_PATH}/modules" .
+}
+
+###############################################################################
+# get depends of ko
+# 1 - Platform
+# 2 - Kernel Version
+# 3 - ko name
+function getdepends() {
+  function _getdepends() {
+    if [ -f "${TMP_PATH}/modules/${1}.ko" ]; then
+      depends=($(modinfo "${TMP_PATH}/modules/${1}.ko" | grep depends: | awk -F: '{print $2}' | awk '$1=$1' | sed 's/,/ /g'))
+      if [ ${#depends[*]} -gt 0 ]; then
+        for k in ${depends[@]}; do
+          echo "${k}"
+          _getdepends "${k}"
+        done
+      fi
+    fi
+  }
+  PLATFORM=${1}
+  KVER=${2}
+  KONAME=${3}
+  # Unzip modules for temporary folder
+  rm -rf "${TMP_PATH}/modules"
+  mkdir -p "${TMP_PATH}/modules"
+  tar -zxf "${MODULES_PATH}/${PLATFORM}-${KVER}.tgz" -C "${TMP_PATH}/modules"
+  DPS=($(_getdepends ${KONAME} | tr ' ' '\n' | sort -u))
+  echo ${DPS[@]}
+  rm -rf "${TMP_PATH}/modules"
 }
