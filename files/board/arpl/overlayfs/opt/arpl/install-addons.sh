b7a007288c5f7ccf34466a2fb0a9861b600ec3af
AuxXxilium <info@auxxxilium.tech>
2023-07-01 17:40:33 +0200

    arc: install-addons: rewrite
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- files/board/arpl/overlayfs/opt/arpl/install-addons.sh
+++ files/board/arpl/overlayfs/opt/arpl/install-addons.sh
@@ -5,7 +5,7 @@ set -e
 . /opt/arpl/include/functions.sh
 
 # Detect if has new local plugins to install/reinstall
-for F in `ls ${CACHE_PATH}/*.addon 2>/dev/null`; do
+for F in $(ls ${CACHE_PATH}/*.addon 2>/dev/null); do
   ADDON=`basename "${F}" | sed 's|.addon||'`
   rm -rf "${ADDONS_PATH}/${ADDON}"
   mkdir -p "${ADDONS_PATH}/${ADDON}"
