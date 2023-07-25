3f9e9a97591b9e7d5ab0bd06719adcdc7ca332a0
AuxXxilium <info@auxxxilium.tech>
2023-07-25 21:35:40 +0200

    arc: more rewrite
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- files/board/arpl/overlayfs/opt/arpl/include/network.sh
+++ files/board/arpl/overlayfs/opt/arpl/include/network.sh
@@ -27,18 +27,18 @@ function getnet() {
       fi
     done
     while true; do
-      dialog --clear --backtitle "`backtitle`" \
+      dialog --clear --backtitle "$(backtitle)" \
         --menu "Network: MAC for 1. NIC" 0 0 0 \
         --file "${TMP_PATH}/opts" \
-      2>${TMP_PATH}/resp
+      2>"${TMP_PATH}/resp"
       [ $? -ne 0 ] && return
-      resp=$(<${TMP_PATH}/resp)
+      resp=$(<"${TMP_PATH}/resp")
       [ -z "${resp}" ] && return
       MAC="${resp}"
       writeConfigKey "cmdline.mac1" "${MAC}" "${USER_CONFIG_FILE}"
       break
     done
-    dialog --backtitle "`backtitle`" \
+    dialog --backtitle "$(backtitle)" \
       --title "Arc Network" --infobox "Set MAC for first NIC" 0 0
     sleep 2
   fi
