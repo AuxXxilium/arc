11934be18012bf6ffd926cb83153e1fb61d5897e
AuxXxilium <info@auxxxilium.tech>
2023-09-01 19:02:01 +0200

    external: update r8169
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- external/r8169/r8169.mk
+++ external/r8169/r8169.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-R8169_VERSION = 6699b34dd60a34cde2ffc8587f6eb0587173dca7
+R8169_VERSION = d555e4a0007d94c6dba87379e345f7f5a00fcf30
 R8169_SITE = $(call github,AuxXxilium,r8169,$(R8169_VERSION))
 R8169_LICENSE = GPL-2.0
 
