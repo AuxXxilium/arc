a7c4a9ca8167030a71534e8241b1ea75c8da0cff
AuxXxilium <info@auxxxilium.tech>
2023-09-02 11:08:20 +0200

    img-gen: update local compile
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- img-gen.sh
+++ img-gen.sh
@@ -7,43 +7,40 @@ if [ ! -d .buildroot ]; then
   git clone --single-branch -b 2023.02.x https://github.com/buildroot/buildroot.git .buildroot
 fi
 # Remove old files
-rm -rf ".buildroot/output/target/opt/arpl"
-rm -rf ".buildroot/board/arpl/overlayfs"
-rm -rf ".buildroot/board/arpl/p1"
-rm -rf ".buildroot/board/arpl/p3"
+rm -rf ".buildroot/output/target/opt/arc"
+rm -rf ".buildroot/board/arc/overlayfs"
+rm -rf ".buildroot/board/arc/p1"
+rm -rf ".buildroot/board/arc/p3"
 
 # Get extractor, LKM, Addons and Modules
 
 . ./scripts/func.sh
 
-getExtractor "files/board/arpl/p3/extractor"
-getLKMs "files/board/arpl/p3/lkms"
-getAddons "files/board/arpl/p3/addons"
-getExtensions "files/board/arpl/p3/extensions"
-getModules "files/board/arpl/p3/modules"
-getConfigs "files/board/arpl/p3/configs"
+getExtractor "files/board/arc/p3/extractor"
+getLKMs "files/board/arc/p3/lkms"
+getAddons "files/board/arc/p3/addons"
+getExtensions "files/board/arc/p3/extensions"
+getModules "files/board/arc/p3/modules"
+getConfigs "files/board/arc/p3/configs"
 
 echo "Extractor, LKM, Addons and Modules loaded"
 
 # Copy files
 echo "Copying files"
 VERSION=$(date +'%y.%-m.dev')
-rm -f files/board/arpl/p1/ARPL-VERSION
+rm -f files/board/arc/p1/ARC-VERSION
 rm -f VERSION
-echo "${VERSION}" >files/board/arpl/p1/ARPL-VERSION
+echo "${VERSION}" >files/board/arc/p1/ARC-VERSION
 echo "${VERSION}" >VERSION
-sed 's/^ARPL_VERSION=.*/ARPL_VERSION="'${VERSION}'"/' -i files/board/arpl/overlayfs/opt/arpl/include/consts.sh
+sed 's/^ARC_VERSION=.*/ARC_VERSION="'${VERSION}'"/' -i files/board/arc/overlayfs/opt/arc/include/consts.sh
 cp -rf files/* .buildroot/
 
 cd .buildroot
 echo "Generating default config"
-make BR2_EXTERNAL=../external -j$(nproc) arpl_defconfig
+make BR2_EXTERNAL=../external -j$(nproc) arc_defconfig
 echo "Version: ${VERSION}"
 echo "Building... Drink a coffee and wait!"
 make BR2_EXTERNAL=../external -j$(nproc)
 cd -
-rm -f arc.img
-cp -f arpl.img arc.img
-rm -f arpl.img
 qemu-img convert -O vmdk arc.img arc-dyn.vmdk
 qemu-img convert -O vmdk -o adapter_type=lsilogic arc.img -o subformat=monolithicFlat arc.vmdk
