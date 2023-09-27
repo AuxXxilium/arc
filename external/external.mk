0bda91bed6c794160f0778384e7acfaab2f7f5a4
AuxXxilium <info@auxxxilium.tech>
2023-09-27 17:05:26 +0200

    external: rework realtek repos
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- external/external.mk
+++ external/external.mk
@@ -1,1 +1,1 @@
-include $(sort $(wildcard $(BR2_EXTERNAL_ARPL_PATH)/*/*.mk))
+include $(sort $(wildcard $(BR2_EXTERNAL_ARC_PATH)/*/*.mk $(BR2_EXTERNAL_ARC_PATH)/*/*/*.mk $(BR2_EXTERNAL_ARC_PATH)/*/*/*/*.mk))
--- external/r8169/r8169.mk
+++ external/r8169/r8169.mk
@@ -4,9 +4,12 @@
 #
 ################################################################################
 
-R8169_VERSION = ffebd251be5440e0013a2435014a2f3ab49371ee
-R8169_SITE = $(call github,wjz304,r8169,$(R8169_VERSION))
+R8169_VERSION = d555e4a0007d94c6dba87379e345f7f5a00fcf30
+R8169_SITE = $(call github,AuxXxilium,r8169,$(R8169_VERSION))
 R8169_LICENSE = GPL-2.0
+
+R8169_MODULE_MAKE_OPTS = \
+    USER_EXTRA_CFLAGS="-DCONFIG_$(call qstrip,$(BR2_ENDIAN))_ENDIAN -Wno-error"
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
