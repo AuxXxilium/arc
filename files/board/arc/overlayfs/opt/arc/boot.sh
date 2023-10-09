1628b08b322bebf9734157c94da1f26aeb5010ab
AuxXxilium <info@auxxxilium.tech>
2023-10-09 16:52:40 +0200

    boot: rework cmdline
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- files/board/arc/overlayfs/opt/arc/boot.sh
+++ files/board/arc/overlayfs/opt/arc/boot.sh
@@ -127,9 +127,9 @@ grep -q "force_junior" /proc/cmdline && CMDLINE_LINE+="force_junior "
 [ ${EFI} -eq 1 ] && CMDLINE_LINE+="withefi " || CMDLINE_LINE+="noefi "
 [ ! "${BUS}" = "usb" ] && CMDLINE_LINE+="synoboot_satadom=${DOM} dom_szmax=${SIZE} "
 if [ "$MACSYS" = "new" ]; then
-  CMDLINE_LINE+="console=ttyS0,115200n8 earlyprintk earlycon=uart8250,io,0x3f8,115200n8 root=/dev/md0 net.ifnames=0 skip_vender_mac_interfaces=0,1,2,3,4,5,6,7 loglevel=15 log_buf_len=32M"
+  CMDLINE_LINE+="console=ttyS0,115200n8 earlyprintk earlycon=uart8250,io,0x3f8,115200n8 root=/dev/md0 skip_vender_mac_interfaces=0,1,2,3,4,5,6,7 loglevel=15 log_buf_len=32M"
 elif [ "$MACSYS" = "old" ]; then
-  CMDLINE_LINE+="console=ttyS0,115200n8 earlyprintk earlycon=uart8250,io,0x3f8,115200n8 root=/dev/md0 net.ifnames=0 loglevel=15 log_buf_len=32M"
+  CMDLINE_LINE+="console=ttyS0,115200n8 earlyprintk earlycon=uart8250,io,0x3f8,115200n8 root=/dev/md0 loglevel=15 log_buf_len=32M"
 fi
 for KEY in ${!CMDLINE[@]}; do
   VALUE="${CMDLINE[${KEY}]}"
