408e38c2acb4de919e38586829ca1aa0a5b2877d
AuxXxilium <info@auxxxilium.tech>
2023-10-30 16:24:31 +0100

    reboot: fix
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- files/board/arc/overlayfs/usr/sbin/loader-reboot.sh
+++ files/board/arc/overlayfs/usr/sbin/loader-reboot.sh
@@ -1,15 +1,25 @@
-#!/usr/bin/env ash
+#!/usr/bin/env bash
 
 function use() {
   echo "Use: ${0} junior|config"
   exit 1
 }
 
+# Sanity checks
+if [ ! ${USER} = "root" ]; then
+  exec sudo $0 $@
+fi
 [ -z "${1}" ] && use
-[ "${1}" != "junior" ] && [ "${1}" != "config" ] && use
+[ ! "${1}" = "junior" -a ! "${1}" = "config" ] && use
 echo "Rebooting to ${1} mode"
-GRUBPATH="$(dirname $(find ${BOOTLOADER_PATH}/ -name grub.cfg | head -1))"
+echo 1 > /proc/sys/kernel/syno_install_flag
+mount /dev/synoboot1 /mnt
+GRUBPATH="$(dirname $(find /mnt/ -name grub.cfg | head -1))"
 ENVFILE="${GRUBPATH}/grubenv"
-[ ! -f "${ENVFILE}" ] && grub-editenv "${ENVFILE}" create
-grub-editenv "${ENVFILE}" set next_entry="${1}"
-reboot
+[ ! -f "${ENVFILE}" ] && grub-editenv ${ENVFILE} create
+
+grub-editenv ${ENVFILE} set next_entry="${1}"
+umount /mnt
+[ -x /usr/syno/sbin/synopoweroff ] && \
+  /usr/syno/sbin/synopoweroff -r ||
+  reboot
