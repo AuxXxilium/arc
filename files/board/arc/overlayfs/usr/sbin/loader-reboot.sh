28abc8f9e91a7e06e77e1af54803f36070d98f05
AuxXxilium <info@auxxxilium.tech>
2023-10-30 19:00:31 +0100

    loader-reboot: fix codestyle
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- files/board/arc/overlayfs/usr/sbin/loader-reboot.sh
+++ files/board/arc/overlayfs/usr/sbin/loader-reboot.sh
@@ -10,7 +10,7 @@ if [ ! ${USER} = "root" ]; then
   exec sudo $0 $@
 fi
 [ -z "${1}" ] && use
-[ ! "${1}" = "junior" -a ! "${1}" = "config" ] && use
+[ ! "${1}" = "junior" ] && [ ! "${1}" = "config" ] && use
 echo "Rebooting to ${1} mode"
 echo 1 > /proc/sys/kernel/syno_install_flag
 mount /dev/synoboot1 /mnt
