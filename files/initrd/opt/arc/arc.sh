47208226fcc55b1475041931cccd415ff233d61c
AuxXxilium <info@auxxxilium.tech>
2023-12-21 21:01:43 +0100

    arc: format build step
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- files/initrd/opt/arc/arc.sh
+++ files/initrd/opt/arc/arc.sh
@@ -535,9 +535,11 @@ function make() {
   if [[ -z "${BOOTCOUNT}" || ${BOOTCOUNT} -gt 0 ]]; then
     writeConfigKey "arc.bootcount" "0" "${USER_CONFIG_FILE}"
   fi
-  clear
-  livepatch
-  sleep 3
+  (
+    livepatch
+    sleep 3
+  ) 2>&1 | dialog --backtitle "$(backtitle)" --colors --title "Build Loader" \
+    --progressbox "Build Bootimage..." 20 70
   if [[ -f "${ORI_ZIMAGE_FILE}" && -f "${ORI_RDGZ_FILE}" && -f "${MOD_ZIMAGE_FILE}" && -f "${MOD_RDGZ_FILE}" ]]; then
     # Build is done
     writeConfigKey "arc.version" "${ARC_VERSION}" "${USER_CONFIG_FILE}"
