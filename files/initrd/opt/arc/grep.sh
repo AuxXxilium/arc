8b623f7b6e3813cbf706c95bd1b6a9669af210f3
AuxXxilium <info@auxxxilium.tech>
2024-01-14 11:36:16 +0100

    grep: rework
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- files/initrd/opt/arc/grep.sh
+++ files/initrd/opt/arc/grep.sh
@@ -1,19 +1,6 @@
 #!/usr/bin/env bash
 
-[[ -z "${ARC_PATH}" || ! -d "${ARC_PATH}/include" ]] && ARC_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-
-. ${ARC_PATH}/include/functions.sh
-
-if findAndMountDSMRoot; then
-  if [ -f "${DSMROOT_PATH}/usr/log/dmesg.txt" ]; then
-    cp -f "${DSMROOT_PATH}/usr/log/dmesg.txt" "/tmp/dmesg.txt"
-    LOG="$(cat /tmp/dmesg.txt)"
-    echo -e "${LOG}"
-    echo
-    echo "Logfile can be found at /tmp/dmesg.txt"
-  else
-    echo "Can't find Logfile"
-  fi
-else
-  echo "Can't find DSM Partition"
-fi
+cd /tmp
+tar cfvz log.tar.gz /mnt/p1/logs/
+cd /opt/arc
+echo "Logs can be found at /tmp/log.tar.gz"
