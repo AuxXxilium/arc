9df4f3619bbee8557e9256df7e1ec5e02b2995a0
AuxXxilium <info@auxxxilium.tech>
2024-02-17 16:32:58 +0100

    boot: remove space
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- files/initrd/opt/arc/boot.sh
+++ files/initrd/opt/arc/boot.sh
@@ -232,7 +232,7 @@ elif [ "${DIRECTBOOT}" = "false" ]; then
   echo -e " \033[1;37m"Booting DSM..."\033[0m"
   for T in $(w | grep -v "TTY" | awk -F' ' '{print $2}')
   do
-    echo -e "\n \033[1;37mThis interface will not be operational. Wait a few minutes.\033[0m\n  Use \033[1;34mhttp://${IPCON}:5000\033[0m or try \033[1;34mhttp://find.synology.com/ \033[0mto find DSM and proceed.\n" >"/dev/${T}" 2>/dev/null || true
+    echo -e "\n\033[1;37mThis interface will not be operational. Wait a few minutes.\033[0m\n  Use \033[1;34mhttp://${IPCON}:5000\033[0m or try \033[1;34mhttp://find.synology.com/ \033[0mto find DSM and proceed.\n" >"/dev/${T}" 2>/dev/null || true
   done
 
   # Clear logs for dbgutils addons
