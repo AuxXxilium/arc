ecf9bc8f74fbc916814661570908bf32c1f57269
AuxXxilium <info@auxxxilium.tech>
2024-06-27 22:11:14 +0200

    init: add dhcp restart (only for testing)
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- files/initrd/opt/arc/init.sh
+++ files/initrd/opt/arc/init.sh
@@ -82,6 +82,7 @@ ETHX="$(ls /sys/class/net/ 2>/dev/null | grep eth)"
 if arrayExistItem "sortnetif:" $(readConfigMap "addons" "${USER_CONFIG_FILE}"); then
   _sort_netif "$(readConfigKey "addons.sortnetif" "${USER_CONFIG_FILE}")"
 fi
+/etc/init.d/S41dhcpcd restart
 # Read/Write IP/Mac config
 for ETH in ${ETHX}; do
   MACR="$(cat /sys/class/net/${ETH}/address 2>/dev/null | sed 's/://g' | tr '[:upper:]' '[:lower:]')"
