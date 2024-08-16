b30456aa91ff68ee6fd5a5185bdc2534bb6d51f4
AuxXxilium <info@auxxxilium.tech>
2024-08-16 23:56:55 +0200

    compat: cleanup
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- files/initrd/opt/arc/include/compat.sh
+++ files/initrd/opt/arc/include/compat.sh
@@ -1,7 +1,3 @@
-[[ -z "${ARC_PATH}" || ! -d "${ARC_PATH}/include" ]] && ARC_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")/../" 2>/dev/null && pwd)"
-
-. ${ARC_PATH}/include/functions.sh
-
 ###############################################################################
 # Compatibility boot
 
