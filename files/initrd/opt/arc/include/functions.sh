f8f8950799459685695b6e59f08d648bd4de6a8e
AuxXxilium <info@auxxxilium.tech>
2024-08-19 18:26:23 +0200

    arc: remove geoblocking for now <- until the next ... comes around
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- files/initrd/opt/arc/include/functions.sh
+++ files/initrd/opt/arc/include/functions.sh
@@ -608,14 +608,6 @@ function offlineCheck() {
   fi
   writeConfigKey "arc.nic" "${ARCNIC}" "${USER_CONFIG_FILE}"
   writeConfigKey "arc.offline" "${OFFLINE}" "${USER_CONFIG_FILE}"
-  local BLOCK="$(curl -m 10 -v "http://ip-api.com/line?fields=countryCode" 2>/dev/null | tr '[:upper:]' '[:lower:]')"
-  if [ "${BLOCK}" == "ua" ]; then
-    dialog --backtitle "$(backtitle)" --title "Blocked Country" \
-      --infobox "You are located in a blocked Country and not authorized to use this Loader!" 5 80
-    rm -rf "${CONFIG_PATH}"
-    sleep 5
-    exit 1
-  fi
 }
 
 ###############################################################################
