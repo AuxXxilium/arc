30e646fc9e5abc5b0ed221204e6d2fc46b178fee
AuxXxilium <info@auxxxilium.tech>
2024-08-20 07:36:40 +0200

    functions: fix syntax

--- files/initrd/opt/arc/include/functions.sh
+++ files/initrd/opt/arc/include/functions.sh
@@ -549,7 +549,7 @@ function ntpCheck() {
     /etc/init.d/S49ntpd restart > /dev/null 2>&1
     hwclock -w > /dev/null 2>&1
   fi
-  if [ -z "${LAYOUT}"]; then
+  if [ -z "${LAYOUT}" ]; then
     [ -n "${KEYMAP}" ] && KEYMAP="$(echo ${KEYMAP} | tr '[:upper:]' '[:lower:]' | tr -d '[:space:]' | tr -d '[:punct:]' | tr -d '[:digit:]')"
     [ -n "${KEYMAP}" ] && writeConfigKey "keymap" "${KEYMAP}" "${USER_CONFIG_FILE}"
     [ -z "${KEYMAP}" ] && KEYMAP="us"
