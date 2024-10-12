745d4d49a69b716d1d494698db123d3c878b4e08
AuxXxilium <info@auxxxilium.tech>
2024-10-12 17:20:35 +0200

    arc: merge stable and next
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- img-gen.sh
+++ img-gen.sh
@@ -9,7 +9,7 @@ sudo git clean -fdx
 
 # Get extractor, LKM, addons and Modules
 getTheme "files/p1/boot/grub"
-getBuildroots "latest" "brs"
+getBuildrootx "latest" "brx"
 
 # Sbase
 IMAGE_FILE="arc.img"
@@ -27,7 +27,7 @@ mkdir -p "/tmp/p3"
 sudo mount ${LOOPX}p1 "/tmp/p1"
 sudo mount ${LOOPX}p3 "/tmp/p3"
 
-[[ ! -f "brs/bzImage-arc" || ! -f "brs/initrd-arc" ]] && return 1
+[[ ! -f "brx/bzImage-arc" || ! -f "brx/initrd-arc" ]] && return 1
 
 VERSION=$(date +'%y.%m.dev')
 echo "${VERSION}" >files/p1/ARC-BASE-VERSION
@@ -36,8 +36,8 @@ echo "stable" >files/p1/ARC-BRANCH
 sed 's/^ARC_BASE_VERSION=.*/ARC_BASE_VERSION="'${VERSION}'"/' -i files/initrd/opt/arc/include/base_consts.sh
 
 echo "Repack initrd"
-cp -f "brs/bzImage-arc" "files/p3/bzImage-arc"
-repackInitrd "brs/initrd-arc" "files/initrd" "files/p3/initrd-arc"
+cp -f "brx/bzImage-arc" "files/p3/bzImage-arc"
+repackInitrd "brx/initrd-arc" "files/initrd" "files/p3/initrd-arc"
 
 echo "Copying files"
 sudo cp -Rf "files/p1/"* "/tmp/p1"
@@ -52,6 +52,4 @@ rmdir "/tmp/p3"
 
 sudo losetup --detach ${LOOPX}
 
-qemu-img convert ${IMAGE_FILE} -O vmdk -o adapter_type=lsilogic arc-dyn.vmdk
 qemu-img convert ${IMAGE_FILE} -O vmdk -o adapter_type=lsilogic,subformat=monolithicFlat arc.vmdk
-qemu-img convert ${IMAGE_FILE} -O vhdx -o subformat=dynamic arc.vhdx
