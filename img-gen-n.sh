e03df2dbabf1e4777ce5c7a863e772a05bfa7a70
AuxXxilium <info@auxxxilium.tech>
2024-10-24 19:08:56 +0200

    build: update local build files
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- img-gen-n.sh
+++ img-gen-n.sh
@@ -8,15 +8,16 @@ sudo git clean -fdx
 . scripts/func.sh
 
 # Get extractor, LKM, addons and Modules
-getLKMs "files/p3/lkms"
+echo "Get Dependencies"
 getAddons "files/p3/addons"
 getModules "files/p3/modules"
 getConfigs "files/p3/configs"
 getPatches "files/p3/patches"
 getCustom "files/p3/custom"
+getLKMs "files/p3/lkms"
 getTheme "files/p1/boot/grub"
-getOffline "files/p3/offline"
-getBuildroots "latest" "brs"
+getOffline "files/p3/configs"
+getBuildrootx "brx"
 
 # Sbase
 IMAGE_FILE="arc.img"
@@ -34,17 +35,17 @@ mkdir -p "/tmp/p3"
 sudo mount ${LOOPX}p1 "/tmp/p1"
 sudo mount ${LOOPX}p3 "/tmp/p3"
 
-[[ ! -f "brs/bzImage-arc" || ! -f "brs/initrd-arc" ]] && return 1
+[[ ! -f "brx/bzImage-arc" || ! -f "brx/initrd-arc" ]] && return 1
 
 VERSION=$(date +'%y.%m.dev')
 echo "${VERSION}" >files/p1/ARC-VERSION
 echo "${VERSION}" >VERSION
-echo "stable" >files/p1/ARC-BRANCH
-sed 's/^ARC_VERSION=.*/ARC_VERSION="'${VERSION}'"/' -i files/initrd/opt/arc/include/consts.sh
+echo "dev" >files/p1/ARC-BRANCH
 
 echo "Repack initrd"
-cp -f "brs/bzImage-arc" "files/p3/bzImage-arc"
-repackInitrd "brs/initrd-arc" "files/initrd" "files/p3/initrd-arc"
+cp -f "brx/bzImage-arc" "files/p3/bzImage-arc"
+cp -f "brx/initrd-arc" "files/p3/initrd-arc"
+repackInitrd "brx/initrd-arc" "files/initrd" "files/p3/initrd-arc"
 
 echo "Copying files"
 sudo cp -Rf "files/p1/"* "/tmp/p1"
@@ -59,6 +60,4 @@ rmdir "/tmp/p3"
 
 sudo losetup --detach ${LOOPX}
 
-qemu-img convert ${IMAGE_FILE} -O vmdk -o adapter_type=lsilogic arc-dyn.vmdk
 qemu-img convert ${IMAGE_FILE} -O vmdk -o adapter_type=lsilogic,subformat=monolithicFlat arc.vmdk
-qemu-img convert ${IMAGE_FILE} -O vhdx -o subformat=dynamic arc.vhdx
--- img-gen-s.sh
+++ img-gen-s.sh
@@ -8,18 +8,19 @@ sudo git clean -fdx
 . scripts/func.sh
 
 # Get extractor, LKM, addons and Modules
-getLKMs "files/p3/lkms"
+echo "Get Dependencies"
 getAddons "files/p3/addons"
 getModules "files/p3/modules"
 getConfigs "files/p3/configs"
 getPatches "files/p3/patches"
 getCustom "files/p3/custom"
+getLKMs "files/p3/lkms"
 getTheme "files/p1/boot/grub"
-getOffline "files/p3/offline"
-getBuildrootx "latest" "brx"
+getOffline "files/p3/configs"
+getBuildroots "brs"
 
-# Xbase
-IMAGE_FILE="arc-next.img"
+# Sbase
+IMAGE_FILE="arc.img"
 gzip -dc "files/initrd/opt/arc/grub.img.gz" >"${IMAGE_FILE}"
 fdisk -l "${IMAGE_FILE}"
 
@@ -34,17 +35,17 @@ mkdir -p "/tmp/p3"
 sudo mount ${LOOPX}p1 "/tmp/p1"
 sudo mount ${LOOPX}p3 "/tmp/p3"
 
-[[ ! -f "brx/bzImage-arc" || ! -f "brx/initrd-arc" ]] && return 1
+[[ ! -f "brs/bzImage-arc" || ! -f "brs/initrd-arc" ]] && return 1
 
 VERSION=$(date +'%y.%m.dev')
 echo "${VERSION}" >files/p1/ARC-VERSION
 echo "${VERSION}" >VERSION
-echo "next" >files/p1/ARC-BRANCH
-sed 's/^ARC_VERSION=.*/ARC_VERSION="'${VERSION}'"/' -i files/initrd/opt/arc/include/consts.sh
+echo "dev" >files/p1/ARC-BRANCH
 
 echo "Repack initrd"
-cp -f "brx/bzImage-arc" "files/p3/bzImage-arc"
-repackInitrd "brx/initrd-arc" "files/initrd" "files/p3/initrd-arc"
+cp -f "brs/bzImage-arc" "files/p3/bzImage-arc"
+cp -f "brs/initrd-arc" "files/p3/initrd-arc"
+repackInitrd "brs/initrd-arc" "files/initrd" "files/p3/initrd-arc"
 
 echo "Copying files"
 sudo cp -Rf "files/p1/"* "/tmp/p1"
@@ -59,6 +60,4 @@ rmdir "/tmp/p3"
 
 sudo losetup --detach ${LOOPX}
 
-qemu-img convert ${IMAGE_FILE} -O vmdk -o adapter_type=lsilogic arc-next-dyn.vmdk
-qemu-img convert ${IMAGE_FILE} -O vmdk -o adapter_type=lsilogic,subformat=monolithicFlat arc-next.vmdk
-qemu-img convert ${IMAGE_FILE} -O vhdx -o subformat=dynamic arc-next.vhdx
+qemu-img convert ${IMAGE_FILE} -O vmdk -o adapter_type=lsilogic,subformat=monolithicFlat arc.vmdk
