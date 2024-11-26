21ffcf4cd5d64f64d77ebf871bda335a9315748d
AuxXxilium <info@auxxxilium.tech>
2024-11-26 17:03:44 +0100

    busybox: cleanup
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- files/initrd/opt/arc/boot.sh
+++ files/initrd/opt/arc/boot.sh
@@ -293,7 +293,7 @@ elif [ "${DIRECTBOOT}" = "false" ]; then
     [ -f "${TMP_PATH}/qrcode_boot.png" ] && echo | fbv -acufi "${TMP_PATH}/qrcode_boot.png" >/dev/null 2>/dev/null || true
   fi
 
-  for T in $(busybox w 2>/dev/null | grep -v 'TTY' | awk '{print $2}'); do
+  for T in $(w 2>/dev/null | grep -v 'TTY' | awk '{print $2}'); do
     if [ -w "/dev/${T}" ]; then
       [ -n "${IPCON}" ] && echo -e "Use \033[1;34mhttp://${IPCON}:5000\033[0m or try \033[1;34mhttp://find.synology.com/ \033[0mto find DSM and proceed.\n\n\033[1;37mThis interface will not be operational. Wait a few minutes.\033[0m\n" >"/dev/${T}" 2>/dev/null || echo -e "Try \033[1;34mhttp://find.synology.com/ \033[0mto find DSM and proceed.\n\n\033[1;37mThis interface will not be operational. Wait a few minutes.\nNo IP found.\033[0m\n" >"/dev/${T}" 2>/dev/null
     fi
