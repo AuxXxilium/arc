e9463d990ee470d74cf9d40c1ca48767e6adb25d
AuxXxilium <info@auxxxilium.tech>
2024-12-27 23:15:58 +0100

    tree: update
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- scripts/func.sh
+++ scripts/func.sh
@@ -503,9 +503,8 @@ function copyBuildroot() {
 
 # create arc executable
 function createArc() {
-  ./scripts/make/makeself.sh --current --keep --zstd --chown --threads=0 --quiet --noexec --notemp ../arc/files/initrd/opt/arc arc "Arc Loader" init.sh
+  ./scripts/make/makeself.sh --current --zstd --chown --threads=0 --quiet ../arc/files/initrd/opt/arc arc "Arc Loader" init.sh
   rm -rf "../arc/files/initrd/opt/arc"
   mkdir -p "../arc/files/initrd/opt/arc"
   mv -f "arc" "../arc/files/initrd/opt/arc/"
-  cp -f "./grub.img.gz" "files/initrd/opt/arc/grub.img.gz"
 }
