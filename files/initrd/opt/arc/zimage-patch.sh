51086a49ee3663ac26c6bc8c43e4c12a49db031b
AuxXxilium <info@auxxxilium.tech>
2025-05-04 16:28:58 +0200

    tree: rewrite some logic
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- files/initrd/opt/arc/zimage-patch.sh
+++ files/initrd/opt/arc/zimage-patch.sh
@@ -12,22 +12,22 @@ set -o pipefail # Get exit code from process piped
 rm -f "${MOD_ZIMAGE_FILE}"
 
 KERNEL="$(readConfigKey "kernel" "${USER_CONFIG_FILE}")"
+PLATFORM="$(readConfigKey "platform" "${USER_CONFIG_FILE}")"
+PRODUCTVER="$(readConfigKey "productver" "${USER_CONFIG_FILE}")"
+KVER="$(readConfigKey "platforms.${PLATFORM}.productvers.\"${PRODUCTVER}\".kver" "${P_FILE}")"
+[ "${PLATFORM}" = "epyc7002" ] && KVERP="${PRODUCTVER}-${KVER}" || die "Unsupported platform: ${PLATFORM}"
 if [ "${KERNEL}" = "custom" ]; then
-  echo -n " - Using customized Kernel"
-  PLATFORM="$(readConfigKey "platform" "${USER_CONFIG_FILE}")"
-  PRODUCTVER="$(readConfigKey "productver" "${USER_CONFIG_FILE}")"
-  KVER="$(readConfigKey "platforms.${PLATFORM}.productvers.\"${PRODUCTVER}\".kver" "${P_FILE}")"
-  [ "${PLATFORM}" = "epyc7002" ] && KVERP="${PRODUCTVER}-${KVER}" || die "Unsupported platform: ${PLATFORM}"
+  echo -e ">>> using customized Kernel for ${PLATFORM} (${KVER})"
   # Extract bzImage
   gzip -dc "${CUSTOM_PATH}/bzImage-${PLATFORM}-${KVERP}.gz" >"${MOD_ZIMAGE_FILE}"
 else
-  echo -n " - Patching official Kernel"
+  echo -e ">>> using official Kernel for ${PLATFORM} (${KVER})"
   # Extract vmlinux
   "${ARC_PATH}/bzImage-to-vmlinux.sh" "${ORI_ZIMAGE_FILE}" "${TMP_PATH}/vmlinux" >"${LOG_FILE}" 2>&1 || dieLog
   # Patch boot params and ramdisk check
   "${ARC_PATH}/kpatch" "${TMP_PATH}/vmlinux" "${TMP_PATH}/vmlinux-mod" >"${LOG_FILE}" 2>&1 || dieLog
   # rebuild zImage
-  "${ARC_PATH}/vmlinux-to-bzImage.sh" "${TMP_PATH}/vmlinux-mod" "${MOD_ZIMAGE_FILE}" >"${LOG_FILE}" 2>&1 || dieLog
+  "${ARC_PATH}/vmlinux-to-bzImage.sh" "${TMP_PATH}/vmlinux-mod" "${MOD_ZIMAGE_FILE}" || dieLog
 fi
 
 sync
