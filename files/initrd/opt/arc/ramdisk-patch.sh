07d0336e3e06c12f0dfac8149278588e02d202b3
AuxXxilium <info@auxxxilium.tech>
2026-04-16 19:28:45 +0200

    ramdisk: update to match new lkms
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- files/initrd/opt/arc/ramdisk-patch.sh
+++ files/initrd/opt/arc/ramdisk-patch.sh
@@ -198,7 +198,7 @@ done
 # Extract modules to ramdisk
 [ "${ARC_MODE}" != "dsm" ] && echo -e ">> Ramdisk: install modules"
 installModules "${PLATFORM}" "${KVERP}" "${!MODULES[@]}" || exit 1
-gzip -dc "${LKMS_PATH}/rp-${PLATFORM}-${KVERP}-${LKM}.ko.gz" >"${RAMDISK_PATH}/usr/lib/modules/rp.ko" 2>>"${LOG_FILE}" || exit 1
+gzip -dc "${LKMS_PATH}/rp-${PLATFORM}-${PRODUCTVER}-${KVER}-${LKM}.ko.gz" >"${RAMDISK_PATH}/usr/lib/modules/rp.ko" 2>>"${LOG_FILE}" || exit 1
 
 # Copying modulelist
 if [ -f "${USER_UP_PATH}/modulelist" ]; then
