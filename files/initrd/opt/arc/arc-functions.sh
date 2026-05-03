51b91eab36676a2d3f2844dd8e62a645dbf4d972
AuxXxilium <info@auxxxilium.tech>
2026-05-03 11:41:14 +0200

    arc-functions: fix missing
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- files/initrd/opt/arc/arc-functions.sh
+++ files/initrd/opt/arc/arc-functions.sh
@@ -6,6 +6,10 @@
 # See /LICENSE for more information.
 #
 
+[[ -z "${ARC_PATH}" || ! -d "${ARC_PATH}/include" ]] && ARC_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" 2>/dev/null && pwd)"
+
+. "${ARC_PATH}/include/functions.sh"
+
 ###############################################################################
 # Model Selection
 function arcModel() {
