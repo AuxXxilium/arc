d25315535f32c4dd68d0e20e29ef66408e86dbc6
AuxXxilium <info@auxxxilium.tech>
2026-05-03 12:02:16 +0200

    boot/init: update headline logic
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- files/initrd/opt/arc/init.sh
+++ files/initrd/opt/arc/init.sh
@@ -41,16 +41,13 @@ clear
 COLUMNS=$(ttysize 2>/dev/null | awk '{print $1}')
 COLUMNS=${COLUMNS:-120}
 BANNER="$(figlet -c -w "${COLUMNS}" "Arc Loader")"
-TITLE="Version:"
-TITLE+=" ${ARC_VERSION} (${ARC_BUILD} @ ${ARC_BASE})"
+TITLE="Version: ${ARC_VERSION} (${ARC_BUILD} @ ${ARC_BASE})"
 printf "\033[1;30m%*s\n" ${COLUMNS} ""
 printf "\033[1;30m%*s\033[A\n" ${COLUMNS} ""
 printf "\033[1;34m%*s\033[0m\n" ${COLUMNS} "${BANNER}"
-printf "\033[1;37m%*s\033[0m\n" $(((${#TITLE} + ${COLUMNS}) / 2)) "${TITLE}"
-TITLE="Boot:"
-[ "${EFI}" = "1" ] && TITLE+=" UEFI" || TITLE+=" BIOS"
-TITLE+=" | Device: ${BUS} | Mode: ${ARC_MODE}"
-printf "\033[1;37m%*s\033[0m\n" $(((${#TITLE} + ${COLUMNS}) / 2)) "${TITLE}"
+printf "%*s\n" $(( (${COLUMNS} + ${#TITLE}) / 2 )) "\033[1;37m${TITLE}\033[0m"
+TITLE="Boot: $([ "${EFI}" = "1" ] && echo "UEFI" || echo "BIOS") | Device: ${BUS} | Mode: ${ARC_MODE}"
+printf "%*s\n" $(( (${COLUMNS} + ${#TITLE}) / 2 )) "\033[1;37m${TITLE}\033[0m"
 
 # Check for Config File
 if [ ! -f "${USER_CONFIG_FILE}" ]; then
