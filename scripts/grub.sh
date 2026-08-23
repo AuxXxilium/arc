28a5410c9a8f9ffb21dc752035ea14af284afab8
AuxXxilium <info@auxxxilium.tech>
2026-08-23 11:44:59 +0200

    grub: increase image size to 3850M
    Grows partition 3, which holds the kernels, modules and the DSM cache,
    since p1 and p2 stay fixed at 50M each.

--- scripts/grub.sh
+++ scripts/grub.sh
@@ -52,7 +52,7 @@ popd > /dev/null
 
 # Create GRUB disk image
 rm -f grub.img
-dd if=/dev/zero of=grub.img bs=1M seek=1850 count=0
+dd if=/dev/zero of=grub.img bs=1M seek=3850 count=0
 echo -e "n\np\n1\n\n+50M\nn\np\n2\n\n+50M\nn\np\n3\n\n\na\n1\nw\nq\n" | fdisk grub.img
 fdisk -l grub.img
 
